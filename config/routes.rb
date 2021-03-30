809a307b7025cd8f0c163befa3fa5e5ee6052de7
Nicoleta Ciobanu <ciobanunicoleta25@gmail.com>
2021-03-30 11:50:33 +0800

    index page done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
+
+  # CRUD
+  # CREATE, READ, UPDATE, DELETE
+  # read all the tasks
+  get '/tasks', to: 'tasks#index'
+  # read one task
+  get '/task/:id', to: 'tasks#show'
+  # create a task
+  get '/tasks/new', to: 'tasks#new' #display the form
+  post '/tasks', to: 'tasks#create'
+  # update a task
+  get '/tasks/:id/edit', to: 'tasks#edit'
+  patch '/tasks/:id', to: 'tasks#update'
+  # destroy a task
+  delete '/tasks/:id', to: 'tasks#destroy'
+
+  # resources :tasks
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+tasks = [
+  { title: "mop the floor", details: "mopping sanitizes your floor" },
+  { title: "wash the dishes", details: "dishwashing made easy"},
+  { title: "go shopping", details: "shopping is part of everyday life" },
+  { title: "sweep the floor", details: "push dirt or garbage off it using a brush"}
+]
+
+tasks.each do |task|
+  t = Task.create!(
+    title: task[:title],
+    details: task[:details]
+)
+  puts "created new task: #{t.title} "
+end
+
+puts "created#{Task.count} tasks"
+
+
+
